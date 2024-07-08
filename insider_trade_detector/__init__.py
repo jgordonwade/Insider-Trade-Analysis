1300f7da618f284cc9ddb307ca38d52ff6143182
Gordon Wade <JGordonWade@protonmail.com>
2024-07-08 16:01:50 -0400

    Parallelized the getting of the Form 4 data

--- insider_trade_detector/__init__.py
+++ insider_trade_detector/__init__.py
@@ -10,7 +10,10 @@ from .sec_interface import ticker_to_filing_list
 from .sec_interface import ticker_to_cik
 from .sec_interface import parse_insider_element
 from .sec_interface import filing_to_urls
-from .sec_interface import ticker_to_insider_buys
+from .sec_interface import fetch_single_xml
+from .sec_interface import fetch_sec_xmls
+from .sec_interface import xmls_to_insider_buys
+from .sec_interface import tikcker_list_to_insider_buy_list
 
 from .lateral_prediction_fns import corr_ewm
 from .lateral_prediction_fns import ohlcv_to_z
--- insider_trade_detector/sec_interface.py
+++ insider_trade_detector/sec_interface.py
@@ -1,6 +1,12 @@
+##############################################################################################
 import requests
 import time
-import xml.etree.ElementTree as et
+import xml.etree.ElementTree as ET
+from  datetime import datetime
+import concurrent.futures
+import re
+import pickle
+
 
 import insider_trade_detector as itd
 
@@ -12,7 +18,41 @@ JGWheader = {"User-Agent": "jgordonwade@protonmail.com"}
 cik_dict = None
 
 #------------------------------------------------------------------------------------------------
+def tikcker_list_to_insider_buy_list(symbol_list,earliest_date='1990-12-31'):
+    form4_list = []
+    for tikr in symbol_list:
+        form4_list += itd.ticker_to_filing_list(tikr,earliest_date)
+    len(form4_list)
 
+    individual_url_list = [itd.filing_to_urls(form4)[0] for form4 in form4_list]
+    print(len(individual_url_list))
+
+
+    if 2>1:
+        time_est = len(individual_url_list)/600
+        print(f'This will take at least {len(individual_url_list)/600:0.2f} minutes. Be patient. ')
+        start_time = time.time()
+        xml_list = itd.fetch_sec_xmls(individual_url_list, rate_limit=10)
+        elapsed_time = time.time() - start_time
+
+        print(f'{elapsed_time/60:0.2f} minutes, or {elapsed_time/len(individual_url_list):.3f} seconds per XML URL')
+
+#         print(len(xml_list))
+#         s = sum(len(x) for x in xml_list)
+#         print(f'{s:,}')
+#         #print(xml_list[123])
+#
+#         with open('xml_list_saved.pkl', 'wb') as f:
+#             pickle.dump(xml_list,f)
+#
+#     with open('xml_list_saved.pkl', 'rb') as f:
+#         xml_list = pickle.load(f)
+#     print(len(xml_list))
+
+    insider_buy_list = itd.xmls_to_insider_buys(xml_list)
+
+    return insider_buy_list
+#------------------------------------------------------------------------------------------------
 
 def ticker_to_cik(ticker_to_find):
     global cik_dict
@@ -34,12 +74,14 @@ def ticker_to_cik(ticker_to_find):
     cik = '0' * (10 - len(cik)) + cik
         
     return cik
-#--------------------------------------------------------------------------------------------
+
+#------------------------------------------------------
+
 def parse_insider_element(insider):
     '''
     `insider` is an ElementTree object correpsonding to a given Form 4. 
     
-    This function returs data for any "non-derivative" transactions which are insider open-market buys. 
+    This function returns data for any "non-derivative" transactions which are insider open-market buys. 
     
     It returns this data in the form of a list of dictionaries called `transactions`, 
     each dict haveing these keys:
@@ -59,11 +101,19 @@ def parse_insider_element(insider):
         if 'common' in security_title.lower() and transaction_code.lower()=='p':
             
             transaction_date = trans_objs[k].find('.//transactionDate/value').text
+            #print(ET.tostring(insider, encoding='unicode'))
+            transaction_symbol = insider.find('.//issuerTradingSymbol').text
             transactionShares = trans_objs[k].find('.//transactionAmounts/transactionShares/value').text
-            transaction_price_per_share = trans_objs[k].find('.//transactionAmounts/transactionPricePerShare/value').text
+            
+            transaction_price_per_share_obj = trans_objs[k].find('.//transactionAmounts/transactionPricePerShare/value')
+            if transaction_price_per_share_obj==None:
+                transaction_price_per_share = 0
+            else:
+                transaction_price_per_share = transaction_price_per_share_obj.text
 
             transactions.append({
                  'transactionDate' : transaction_date
+                ,'issuerTradingSymbol' : transaction_symbol
                 ,'transactionShares' : transactionShares
                 ,'transactionPricePerShare' : transaction_price_per_share
             })
@@ -87,85 +137,7 @@ def parse_insider_element(insider):
         return transactions
 
 #------------------------------------------------------------
-def ticker_to_filing_list(symbol:str)->dict:
 
-    # For a given ticker, get the  URL's for the XML versions of the submissions
-
-    # We get a .json file containing information about all 'recent' SEC filings 
-    # that have been made by this CIK/ticker symbol
-
-    # This json file converts cleanly to a dict which I call `response`
-
-    # * `response['filings']['recent']` is a dict with 14 keys. 
-    # The value for each key is a list, and all 14 values-lists are the same length, 
-    # and that length is the number of filings this company has made. 
-
-    # So for example if we create a variable called `filing_num`, then the 
-    # `filing_num`-th entry in each of these 14 lists correspond to what we 
-    # can think of as the `filing_num`-th filing by this company. 
-    # (I emphasize that this `filing_num` is my own variable, not the SEC's!)  
-
-    #   response['filings'][recent']['accessionNumber'] The accession numbers for all this company's SEC fiings. 
-    #   response['filings'][recent']['form'] We're interested in Form 4's, for which this value is simply '4'
-    #   response['filings'][recent']['primaryDocument'] The file name of thej XML version of the actual filing. 
-
-
-    cik = ticker_to_cik(symbol)
-    url = 'https://data.sec.gov/submissions/CIK'+cik+'.json'
-    print('About to get this url: ', url)
-    response = requests.get(url, headers=JGWheader)
-    time.sleep(0.1001)
-    print('Resonse code: ', response.status_code, response.reason)
-    response = response.json()
-
-    recent_filings = response['filings']['recent']
-
-    # `recent filings` is a dict, with the following keys, each of which is a list of strings.
-    #     accessionNumber
-    #     filingDate
-    #     reportDate
-    #     acceptanceDateTime
-    #     act 
-    #     form
-    #     fileNumber
-    #     filmNumber
-    #     items
-    #     size 
-    #     isXBRL
-    #     isInlineXBRL 
-    #     primaryDocument 
-    #     primaryDocDescription 
-
-    # ---------------
-
-    # Go thru `recent_filings` and get the filing_num's of only Form 4 repprts
-    n_filings = len(recent_filings['accessionNumber'])
-    form4_indices=[]
-    for filing_num in range(n_filings):
-        if recent_filings['form'][filing_num] == '4':
-            form4_indices = form4_indices+[filing_num]
-
-
-    # Now build a dict `recent_forms` which is keyed in accession number 
-    # and has the data we want in the keys.        
-    recent_form4s = []
-    count=0
-    for index in form4_indices:
-        accnum =      recent_filings['accessionNumber'][index]
-        trade_date =  recent_filings['reportDate'][index]
-        filing_date = recent_filings['filingDate'][index]
-        xml_name =    recent_filings['primaryDocument'][index]
-
-        recent_form4s.append({'accession_number': accnum,
-                              'ticker': symbol.strip(),
-                              'cik': cik,
-                              'trade_date': trade_date,
-                              'filing_date': filing_date,
-                              'xml_name': xml_name})
-        #print(recent_form4s[count]['accession_number'])
-        count += 1
-    return recent_form4s
-#------------------------------------------------------------
 def filing_to_urls(filing_dict:dict):
     # filing_dict is a dictionary with keys:
     # 'accession_number', 'ticker', 'cik', 'trade_date', 'filing_date', 'xml_name'
@@ -188,86 +160,189 @@ def filing_to_urls(filing_dict:dict):
     txt_url = txt_base.format(cik=cik, accession=accession, accession_full=accession_full)
 
     return [txt_url, xml_url]
-#-------------------------------------------------------------------------
-def ticker_to_insider_buys(ticker)->list:
-    '''
-    - **Input:** A string containing a single  ticker symbol.
 
-    - **Output:** A list (`insider_buys`), where each element is a dictionary containing relevant information about an 
-        insider buy transaction. These data include: accession number, ticker, trade and filing dates, number of shares, 
-        price per share, insider type, and the URL of the XML filing.
+#------------------------------------------------------------
 
-    - **Outline** of  ``ticker_to_insider_buys`
-        - Calls `ticker_to_filing_list(ticker)` to get a list of SEC filings (Form 4s) related to the given ticker. 
-          This list contains information about each filing, like accession number, trade date, filing date, and the 
-          name of the XML file containing the filing's details.
+def ticker_to_filing_list(symbol: str, earliest_date: str = '1999-12-31') -> dict:
 
-        - Iterates through each filing (Form 4) in the list. For each filing, construct URLs for the text and 
-           XML versions of the filing document (`filing_to_urls` function).
-    
-        - Make a “get” request to retrieve the text version of the filing. From this, extracts and trims the 
-          XML portion from the retrieved text. 
+    # For a given ticker, get the  URL's for the XML versions of the submissions
 
-        - Then parses the XML using `itd.parse_insider_element`. This function returns a list of dictionaries, 
-          each representing an insider buy transaction. These transactions are then appended to the `insider_buys` list.
-    '''
-        
-    filing_dict_list = itd.ticker_to_filing_list(ticker)
-    
-    estimated_time = len(filing_dict_list)*0.004146 + 0.104709 
-    print(f'{len(filing_dict_list)} filings. This loop will take about {estimated_time:.2f} minutes')
-    
-    tcount=0
+    # We get a .json file containing information about all 'recent' SEC filings 
+    # that have been made by this CIK/ticker symbol
+
+    # This json file converts cleanly to a dict which I call `response`
+
+    # * `response['filings']['recent']` is a dict with 14 keys. 
+    # The value for each key is a list, and all 14 values-lists are the same length, 
+    # and that length is the number of filings this company has made. 
+
+    # So for example if we create a variable called `filing_num`, then the 
+    # `filing_num`-th entry in each of these 14 lists correspond to what we 
+    # can think of as the `filing_num`-th filing by this company. 
+    # (I emphasize that this `filing_num` is my own variable, not the SEC's!)  
+
+    #   response['filings'][recent']['accessionNumber'] The accession numbers for all this company's SEC fiings. 
+    #   response['filings'][recent']['form'] We're interested in Form 4's, for which this value is simply '4'
+    #   response['filings'][recent']['primaryDocument'] The file name of thej XML version of the actual filing. 
+
+
+    earliest_date = datetime.strptime(earliest_date,'%Y-%m-%d')
+
+    cik = ticker_to_cik(symbol)
+    url = 'https://data.sec.gov/submissions/CIK'+cik+'.json'
+    print('About to get this url: ', url, symbol)
+    response = requests.get(url, headers=JGWheader)
+    time.sleep(0.1001)
+    #print('Resonse code: ', response.status_code, response.reason)
+    response = response.json()
+
+    recent_filings = response['filings']['recent']
+
+    # `recent filings` is a dict, with the following keys, each of which is a list of strings.
+    #     accessionNumber
+    #     filingDate
+    #     reportDate
+    #     acceptanceDateTime
+    #     act 
+    #     form
+    #     fileNumber
+    #     filmNumber
+    #     items
+    #     size 
+    #     isXBRL
+    #     isInlineXBRL 
+    #     primaryDocument 
+    #     primaryDocDescription 
+    # ---------------
+
+    # Go thru `recent_filings` and get the filing_num's of only Form 4 repprts
+    n_filings = len(recent_filings['accessionNumber'])
+    form4_indices=[]
+    for filing_num in range(n_filings):
+        if len(recent_filings['reportDate'][filing_num])>5:
+            trade_date = datetime.strptime(recent_filings['filingDate'][filing_num],'%Y-%m-%d')
+            if recent_filings['form'][filing_num] == '4' and trade_date>earliest_date:
+                form4_indices = form4_indices+[filing_num]
+
+    # Now build a dict `recent_forms` which is keyed in accession number 
+    # and has the data we want in the keys.        
+    recent_form4s = []
+    count=0
+    for index in form4_indices:
+        accnum =      recent_filings['accessionNumber'][index]
+        trade_date =  recent_filings['reportDate'][index]
+        filing_date = recent_filings['filingDate'][index]
+        xml_name =    recent_filings['primaryDocument'][index]
+
+        recent_form4s.append({'accession_number': accnum,
+                              'ticker': symbol.strip(),
+                              'cik': cik,
+                              'trade_date': trade_date,
+                              'filing_date': filing_date,
+                              'xml_name': xml_name})
+        #print(recent_form4s[count]['accession_number'])
+        count += 1
+    return recent_form4s
+
+#------------------------------------------------
+def fetch_single_xml(url, headers):
+    """
+    Fetch a single XML from the given URL.
+
+    Parameters:
+    url (str): URL to fetch the XML data from.
+    headers (dict): Headers to use in the request.
+
+    Returns:
+    tuple: URL and the XML content as a string (None if the request fails).
+    """
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return url, response.text
+    else:
+        print(f"Failed to fetch {url}: Status code {response.status_code}")
+        return url, None
+
+#-------------------------------------------------------------
+
+def fetch_sec_xmls(individual_url_list, rate_limit=10):
+    """
+    Fetch XML data from SEC API with throttling.
+
+    Parameters:
+    individual_url_list (list): List of URLs to fetch the XML data from.
+    rate_limit (int): Number of requests allowed per second.
+
+    Returns:
+    list: List of XML contents as strings.
+    """
+    xml_list = []
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=rate_limit) as executor:
+        future_to_url = {executor.submit(fetch_single_xml, url, headers=JGWheader): url for url in individual_url_list}
+        for future in concurrent.futures.as_completed(future_to_url):
+            url, xml_content = future.result()
+            if xml_content:
+                xml_list.append(xml_content)
+            
+            loop_count = len(xml_list)
+            if loop_count % 100 == 0:
+                print(f'\t {100 * loop_count / len(individual_url_list):.2f} percent done')
+                
+            # Throttle requests to rate_limit per second
+            time.sleep(1 / rate_limit)
+
+    return xml_list
+
+#-----------------------------------------------
+def xmls_to_insider_buys(xml_list):
     insider_buys = []
-    stime = time.time()
-    for form4_dict in filing_dict_list:
-        '''
-        form4_dict has keys: 
-            'accession_number'
-            'ticker'
-            'cik'
-            'trade_date'
-            'filing_date'
-            'xml_name'
-        '''
+    for response in xml_list:
 
-        [txt_addr, xml_addr] = filing_to_urls(form4_dict)
+        pattern = r'ACCESSION NUMBER:\s+(\d{10}-\d{2}-\d{6})'
+        match = re.search(pattern, response)
+        if match:
+            accession_number = match.group(1)
+        else:
+            print("Accession Number not found")
+                
 
-        response = requests.get(txt_addr,headers=JGWheader)
-        xml_time_start = time.time()
-        tcount += 1
-        if tcount%50 == 0:
-            print(f'{tcount/len(filing_dict_list):0.3f} of the way thru')
+        match = re.search(r"<ACCEPTANCE-DATETIME>(\d{14})", response)
+        if match:
+            date_time_str = match.group(1)
+            filing_date = f"{date_time_str[:4]}-{date_time_str[4:6]}-{date_time_str[6:8]}"
+            
+            #date_time_obj = datetime.strptime(date_time_str, "%Y%m%d%")
+            #date_time_obj = datetime.strptime(date_time_str, "%Y%m%d%H%M%S")
 
-        if response.status_code==200:
-            response = response.text
-            xml_start = 5+response.find('<XML>')
-            xml_end = response.find('</XML>')
-            xml_str = response[xml_start:xml_end]
-            xml_str = xml_str.strip()
+        else:
+            print("Date-time string not found in the response.")
+        
+        xml_start = 5+response.find('<XML>')
+        xml_end = response.find('</XML>')
+        xml_str = response[xml_start:xml_end]
+        xml_str = xml_str.strip()
+        
+        form4_root = ET.fromstring(xml_str)
 
-            form4_root = et.fromstring(xml_str)
-
-            transactions = itd.parse_insider_element(form4_root)
-
-            for k in range(len(transactions)):
-                insider_buys.append({
-                    'accession': form4_dict['accession_number'],
-                    'ticker': form4_dict['ticker'],
-                    'trade_date': transactions[k]['transactionDate'],
-                    'filing_date' : form4_dict['filing_date'],
-                    'insider_type': transactions[k]['insider_type'],
-                    'number_shares': transactions[k]['transactionShares'],
-                    'price_per_share' : transactions[k]['transactionPricePerShare'],
-                    'insider_type' : transactions[k]['insider_type'],
-                    'xml_url' : xml_addr
+        transactions = parse_insider_element(form4_root)
+        #print(transactions)
+        
+        for k in range(len(transactions)):
+            ticker = transactions[k]['issuerTradingSymbol']
+            insider_buys.append({
+                'accession': accession_number,
+                'ticker': ticker,
+                'trade_date': transactions[k]['transactionDate'],
+                'filing_date' : filing_date,
+                'insider_type': transactions[k]['insider_type'],
+                'number_shares': transactions[k]['transactionShares'],
+                'price_per_share' : transactions[k]['transactionPricePerShare'],
+                'insider_type' : transactions[k]['insider_type'],
                 })
 
-        xml_time_sleep = 0.1001 - (time.time()-xml_time_start)
-        time.sleep(max([0,xml_time_sleep]))
 
-    print(f'Done. Took {(time.time()-stime)/60:0.2f} minutes')
-    
     return insider_buys
-#-----------------------------------------------------------------------------------------------
+
+######################################################
 
